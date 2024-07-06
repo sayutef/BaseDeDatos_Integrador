16b166f539e99d2cb9070546401f383329bed307
Miltonvaz <233427@ids.upchiapas.edu.mx>
2024-07-05 19:49:09 -0600

    update

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,48 @@
-  import express, { Application } from 'express';
-  import bodyParser from 'body-parser';
-  import * as dotenv from 'dotenv';
-  import userRoutes from './User/routes/userRoutes';
-  import { errorHandler } from './shared/middlewares/errorHandlers';
-  import { notFoundHandler } from './shared/middlewares/notFoundHandlers';
-  import productRoutes from './product/routes/productRoutes';
-  import clientRoutes from './client/routes/clientRoutes';
-  import purchaseOrderRoutes from './purchase_order/routes/purchaseOrderRoutes';
-  import deliveryRoutes from './delivery/routes/deliveryRoutes';
+import express, { Application } from 'express';
+import bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
+import userRoutes from './User/routes/userRoutes';
+import { errorHandler } from './shared/middlewares/errorHandlers';
+import { notFoundHandler } from './shared/middlewares/notFoundHandlers';
+import productRoutes from './product/routes/productRoutes';
+import clientRoutes from './client/routes/clientRoutes';
+import purchaseOrderRoutes from './purchase_order/routes/purchaseOrderRoutes';
+import deliveryRoutes from './delivery/routes/deliveryRoutes';
 import roleRoutes from './role/routes/roleRoutes';
+import https from 'https';
+import fs from 'fs';
 
-  dotenv.config();
 
-  const app: Application = express();
-  const port: number = parseInt(process.env.PORT as string, 10);
+dotenv.config();
 
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  
-  app.use('/api/rol', roleRoutes); 
-  app.use('/api/users', userRoutes); 
-  app.use('/api/products', productRoutes); 
-  app.use('/api/clients', clientRoutes);
-  app.use('/api/purchaseOrders', purchaseOrderRoutes);
-  app.use('/api/deliverys', deliveryRoutes);
 
-  app.use(notFoundHandler);
-  app.use(errorHandler);
+const app: Application = express();
 
-  app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
-  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use('/api/rol', roleRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/clients', clientRoutes);
+app.use('/api/purchaseOrders', purchaseOrderRoutes);
+app.use('/api/deliverys', deliveryRoutes);
+
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/ferreteriaapi.integrador.xyz/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/ferreteriaapi.integrador.xyz/fullchain.pem')
+};
+
+
+const port: number = parseInt(process.env.PORT as string, 10) || 3000;
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`Servidor HTTPS corriendo en el puerto ${port}`);
+});
