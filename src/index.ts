9c30fcf9298b695cb0dec18a688bb113768abf58
Miltonvaz <233427@ids.upchiapas.edu.mx>
2024-07-09 19:23:58 -0600

    update

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,24 @@ import deliveryRoutes from './delivery/routes/deliveryRoutes';
 import roleRoutes from './role/routes/roleRoutes';
 import https from 'https';
 import fs from 'fs';
-
+import cors from 'cors';
 
 dotenv.config();
 
-
-
 const app: Application = express();
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// CORS configuration
+const corsOptions = {
+  origin: ['https://ferreteriaapi.integrador.xyz', 'https://ferreteriapi.integrador.xyz'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 
 app.use('/api/rol', roleRoutes);
 app.use('/api/users', userRoutes);
@@ -31,19 +37,21 @@ app.use('/api/clients', clientRoutes);
 app.use('/api/purchaseOrders', purchaseOrderRoutes);
 app.use('/api/deliverys', deliveryRoutes);
 
-
 app.use(notFoundHandler);
 app.use(errorHandler);
 
+// Route for testing CORS
+app.get('/', (_req, res) => {
+  res.send('CORS configurado correctamente!');
+});
+
+const PORT = parseInt(process.env.PORT as string, 10) || 3000;
 
 const options = {
   key: fs.readFileSync('privkey.pem'),
   cert: fs.readFileSync('fullchain.pem')
 };
 
-
-const port: number = parseInt(process.env.PORT as string, 10) || 3000;
-
-https.createServer(options, app).listen(port, () => {
-  console.log(`Servidor HTTPS corriendo en el puerto ${port}`);
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Servidor HTTPS corriendo en el puerto ${PORT}`);
 });
